1ca9a9b3d3000d2dd10bb62961ccd891410c146d
Vlada <vladasilaeva@yandex.ru>
2020-02-25 18:07:17 +0300

    коммит чипа 2

--- chips.py
+++ chips.py
@@ -1,48 +1,70 @@
 class Chip:
-    def __init__(self):
-        self.field = [['.'] * 7 for _ in range(7)]
-        self.field[3][3] = 'my_head'
-        self.groups = {'and0': [], 'and1': [], 'and2': [], 'and3': [], 'or1': [], 'or2': []}
-        self.cell_types = {'my_tail': 'x', 'my_body': '-', 'my_head': 'o',
-                           'other_tail': '*', 'other_body': '=', 'other_head': 'q',
-                           'empty': ' ', 'wall': '#', '': '.'}
+    CELL_TYPES = {'my_tail': 'x', 'my_body': '-', 'my_head': 'o',
+                  'other_tail': '*', 'other_body': '=', 'other_head': 'q',
+                  'empty': ' ', 'wall': '#', None: '.'}
+    DEFAULT_BOARD = [[None] * 7 for _ in range(7)]
+    DEFAULT_BOARD[3][3] = 'my_head'
 
-    def add(self, h, w, cell_type, group='and0', *args):
-        h, w = int(h), int(w)
-        if 0 <= h < 7 and 0 <= w < 7 and cell_type in self.cell_types.keys() \
-                and group in self.groups.keys() and not len(args):
+    def __init__(self, board=None, groups_or=None, groups_and=None, groups_ex=None):
+        if board is None or len(board) != 7:
+            board = Chip.DEFAULT_BOARD
+        self.board = board
+        self.groups_and = {'': [], 'and1': [], 'and2': []}
+        self.groups_or = {'or1': [], 'or2': []}
+        self.groups_ex = {'ex1': [], 'ex2': []}
+        if groups_and is not None:
+            for key in self.groups_and:
+                if key in groups_and:
+                    self.groups_and[key] = list(groups_and[key])
+        if groups_or is not None:
+            for key in self.groups_or:
+                if key in groups_or:
+                    self.groups_or[key] = list(groups_or[key])
+        if groups_ex is not None:
+            for key in self.groups_ex:
+                if key in groups_ex:
+                    self.groups_ex[key] = list(groups_ex[key])
+
+    def edit(self, h, w, cell_type, group='', *args):
+        if 0 <= h < 7 and 0 <= w < 7 and cell_type in Chip.CELL_TYPES.keys() \
+                and group in list(self.groups_and.keys()) + list(self.groups_or.keys()) +\
+                list(self.groups_ex.keys()) and not len(args):
             if cell_type == 'my_head':
                 print('--fail--')
                 return
-                # for i in range(len(self.field)):
-                #     if 'o' in self.field[i]:
-                #         self.field[i][self.field[i].index('o')] = '.'
-                #         break
-            self.field[h][w] = self.cell_types[cell_type]
-            for g in self.groups.keys():
-                if (h, w) in self.groups[g]:
-                    del self.groups[g][self.groups[g].index((h, w))]
-            self.groups[group].append((h, w))
+            self.board[h][w] = cell_type
+            for g in self.groups_and.keys():
+                if (h, w) in self.groups_and[g]:
+                    del self.groups_and[g][self.groups_and[g].index((h, w))]
+            for g in self.groups_or.keys():
+                if (h, w) in self.groups_or[g]:
+                    del self.groups_and[g][self.groups_or[g].index((h, w))]
+            for g in self.groups_ex.keys():
+                if (h, w) in self.groups_ex[g]:
+                    del self.groups_and[g][self.groups_ex[g].index((h, w))]
+            if group == '':
+                self.groups_and[group].append((h, w))
+            elif 'and' in group:
+                self.groups_and[group].append((h, w))
+            elif 'or' in group:
+                self.groups_or[group].append((h, w))
+            else:
+                self.groups_ex[group].append((h, w))
         else:
             print('--fail--')
 
     def print(self, with_groups=False):
-        for row in self.field:
+        for row in self.board:
             for cell in row:
-                print(self.cell_types[cell], end=' ')
+                print(Chip.CELL_TYPES[cell], end=' ')
             print()
         if with_groups:
-            for g in self.groups.keys():
-                print(f'{g}: {self.groups[g]}')
+            for g in self.groups_and.keys():
+                print(f'{g}: {self.groups_and[g]}')
+            for g in self.groups_or.keys():
+                print(f'{g}: {self.groups_or[g]}')
+            for g in self.groups_ex.keys():
+                print(f'{g}: {self.groups_ex[g]}')
 
-
-text = '''Напиши координаты (два числа от 0 до 6 через пробел).
-Затем через пробел напиши тип клетки (my/other tail/body/head или empty или wall).
-Через пробел напиши группу (and1, and2, and3, or1, or2). Если клетка не состоит ни в какой из групп, не пиши ничего.'''
-print(text)
-ch = Chip()
-answer = input()
-while answer != 'конец':
-    ch.add(*answer.split())
-    answer = input()
-ch.print(True)
+    def reversed(self):
+        return Chip([row[::-1] for row in self.board])
